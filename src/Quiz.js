755e4a0d1374e2064ece76fed61219b705a3a4e4
Ted Bui <tedtbui@gmail.com>
2019-10-10 14:48:16 -0700

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -20,7 +20,7 @@ class Quiz extends Component {
         return (
             <div>
                 {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
-                showNextQuestion={this.showNextQuestion.bind(this)} />}
+                showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
         );
     }
